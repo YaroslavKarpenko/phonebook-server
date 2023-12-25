83f8fa24a02c50bc4d37bbfcde81d1e49fcfc776
YaroslavKarpenko <yaroslavkarpenko1505@gmail.com>
2023-12-25 16:28:23 +0200

    add validation

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
+const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
-const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.static("dist"));
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "content missing" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
+  }
+  next(error);
+};
 
 app.get("/api/persons/", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
-app.get("/api/persons/:id", (request, response) => {
-  const id = +request.params.id;
-  const person = persons.find((person) => person.id === id);
 
-  person ? response.json(person) : response.status(404).end();
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      person ? response.json(person) : response.status(404).end();
+    })
+    .catch((error) => next(error));
 });
-app.get("/info", (request, response) => {
-  response.send(
-    `<span>Phonebook has info for ${
-      persons.length
-    } people <br/>${Date()}</span>`
-  );
-});
-function generateId() {
-  const min = Math.ceil(0);
-  const max = Math.floor(1e10);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-  const nameExists = persons.find((person) => person.name === body.name);
 
-  if (!body.name || !body.number) {
+app.post("/api/persons", (request, response, next) => {
+  const { name, number } = request.body;
+  if (name === undefined || number === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
-  } else if (nameExists) {
-    return response.status(409).json({ error: "name must be unique" });
   }
-  const person = { name: body.name, number: body.number, id: generateId() };
-  persons = persons.concat(person);
 
-  response.json(person);
+  const person = new Person({
+    name,
+    number,
+  });
+  person
+    .save()
+    .then((savedPerson) => response.json(savedPerson))
+    .catch((error) => next(error));
 });
-app.delete("/api/persons/:id", (request, response) => {
-  const id = +request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((deletedPerson) => {
+      deletedPerson
+        ? response.status(204).end()
+        : response.status(400).json({ error: "content missing" });
+    })
+    .catch((error) => next(error));
 });
-const PORT = process.env.PORT || 3001;
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      updatedPerson
+        ? response.send(updatedPerson)
+        : response.status(400).json({ error: "content missing" });
+    })
+    .catch((error) => next(error));
+});
+app.use(errorHandler);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
